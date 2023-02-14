9e40be40d60615940fce9c92b4246b67932a2e0c
roddyisthebest <bsy17171@naver.com>
2023-02-14 18:59:56 +0900

    [구현] 퍼블리싱

--- App.tsx
+++ App.tsx
@@ -1,119 +1,141 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
+import React, {useState, type PropsWithChildren} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  FlatList,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+// const Section: React.FC<
+//   PropsWithChildren<{
+//     title: string;
+//   }>
+// > = ({children, title}) => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
   };
-
+  const [data, setData] = useState<any[]>([1, 2, 3, 4, 5, 6]);
+  const renderItem = () => <View style={styles.item} />;
   return (
     <SafeAreaView style={backgroundStyle}>
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.text}>헤더영역</Text>
+        </View>
+        <View style={styles.body}>
+          <View style={styles.bodyLabel}>
+            <Text style={styles.text}>가로 스크롤 영역 #1</Text>
+          </View>
+          <FlatList
+            data={data}
+            horizontal
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+            style={styles.flatListStyle}
+            contentContainerStyle={styles.flatListContainerStyle}
+          />
+          <View style={styles.bodyLabel}>
+            <Text style={styles.text}>가로 스크롤 영역 #2</Text>
+          </View>
+          <FlatList
+            data={data}
+            horizontal
+            renderItem={renderItem}
+            ItemSeparatorComponent={() => <View style={styles.separator} />}
+            style={styles.flatListStyle}
+            contentContainerStyle={styles.flatListContainerStyle}
+          />
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.text}>탭바영역</Text>
+        </View>
+      </View>
+
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  section: {
+    flex: 1,
+    borderColor: 'black',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: '#4f4f4f',
   },
-  highlight: {
-    fontWeight: '700',
+  body: {
+    flex: 10,
+  },
+  bodyLabel: {
+    height: 60,
+    width: '100%',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  item: {
+    width: 160,
+    height: 160,
+    backgroundColor: '#3A2F2A',
+    borderRadius: 20,
+  },
+  separator: {
+    width: 15,
+  },
+  flatListStyle: {
+    flexGrow: 0,
+    height: 190,
+    backgroundColor: '#FFE742',
+  },
+  flatListContainerStyle: {
+    padding: 15,
   },
 });
 
